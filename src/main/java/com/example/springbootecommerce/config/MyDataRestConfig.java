f0e4d56a25ef9ba0ed822a229580e87736a3fe29
tomgu <atom8708@gmail.com>
2022-11-06 16:21:01 +0800

    create backend with spring boot and jpa repository

---
+++ src/main/java/com/example/springbootecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,29 @@
+package com.example.springbootecommerce.config;
+
+import com.example.springbootecommerce.entity.Product;
+import com.example.springbootecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+
+        HttpMethod[] unSupportedMethods = {HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE};
+
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(unSupportedMethods)))
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(unSupportedMethods)));
+
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(unSupportedMethods)))
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(unSupportedMethods)));
+    }
+}
