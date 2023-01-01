4a7d74a97e817dcdcbaca9e1a3b508bd75c4042c
tomgu <atom8708@gmail.com>
2023-01-01 10:12:25 +0000

    Stripe payment integration

--- src/main/java/com/example/springbootecommerce/controller/CheckoutController.java
+++ src/main/java/com/example/springbootecommerce/controller/CheckoutController.java
@@ -1,9 +1,14 @@
 package com.example.springbootecommerce.controller;
 
+import com.example.springbootecommerce.dto.PaymentInfo;
 import com.example.springbootecommerce.dto.Purchase;
 import com.example.springbootecommerce.dto.PurchaseResponse;
 import com.example.springbootecommerce.service.CheckoutService;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
@@ -21,6 +26,12 @@ public class CheckoutController {
     @PostMapping("/purchase")
     public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
         return checkoutService.placeOrder(purchase);
+    }
+
+    @PostMapping("/payment-intent")
+    public ResponseEntity<String> createPaymentIntent(@RequestBody PaymentInfo paymentInfo) throws StripeException  {
+        PaymentIntent paymentIntent = checkoutService.createPaymentIntent(paymentInfo);
+        return new ResponseEntity<>(paymentIntent.toJson(), HttpStatus.OK);
     }
 
 }
--- src/main/java/com/example/springbootecommerce/service/CheckoutServiceImpl.java
+++ src/main/java/com/example/springbootecommerce/service/CheckoutServiceImpl.java
@@ -1,18 +1,21 @@
 package com.example.springbootecommerce.service;
 
 import com.example.springbootecommerce.dao.CustomerRepository;
+import com.example.springbootecommerce.dto.PaymentInfo;
 import com.example.springbootecommerce.dto.Purchase;
 import com.example.springbootecommerce.dto.PurchaseResponse;
 import com.example.springbootecommerce.entity.Customer;
 import com.example.springbootecommerce.entity.Order;
 import com.example.springbootecommerce.entity.OrderItem;
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Objects;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 
 @Service
 public class CheckoutServiceImpl implements CheckoutService{
@@ -20,8 +23,11 @@ public class CheckoutServiceImpl implements CheckoutService{
     private CustomerRepository customerRepository;
 
     @Autowired
-    public CheckoutServiceImpl(CustomerRepository customerRepository) {
+    public CheckoutServiceImpl(CustomerRepository customerRepository,
+                               @Value("${stripe.key.secret}") String secretKey) {
         this.customerRepository = customerRepository;
+        // initialize Stripe api key
+        Stripe.apiKey = secretKey;
     }
 
     @Override
@@ -57,6 +63,21 @@ public class CheckoutServiceImpl implements CheckoutService{
         customerRepository.save(customer);
 
         return new PurchaseResponse(orderTrackingNumber);
+    }
+
+    @Override
+    public PaymentIntent createPaymentIntent(PaymentInfo paymentInfo) throws StripeException {
+
+        List<String> paymentMethodTypes = new ArrayList<>();
+        paymentMethodTypes.add("card");
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("amount", paymentInfo.getAmount());
+        params.put("currency", paymentInfo.getCurrency());
+        params.put("payment_method_types", paymentMethodTypes);
+        params.put("receipt_email", paymentInfo.getReceiptEmail());
+
+        return PaymentIntent.create(params);
     }
 
     private String generateOrderTrackingNumber() {
